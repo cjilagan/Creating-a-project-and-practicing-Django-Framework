cb098aab1c9b8753d3b29261491d71bd1e7c3a58
cjilagan <christianjherardilagan@gmail.com>
2025-07-31 01:45:41 +0800

    added url routing and request function

--- projectdjango/pages/views.py
+++ projectdjango/pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Welcome to the Home Page</h1>")
+
+def signup_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Signup Page</h1>")
